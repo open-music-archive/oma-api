8e4f745d4f21a3cb4875dbedf3cb0faa11a81b93
florianthalmann <florianthalmann@bluewin.ch>
2018-06-15 15:45:58 +0100

    put method for adding records, added bodyparser

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,25 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 import * as store from './store';
 
 const PORT = process.env.PORT || 8060;
 
+
 const app = express();
+app.use(bodyParser.json({limit: '50mb'}));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+app.post('/record', (req, res) => {
+  res.send(store.addRecord(req.body));
+});
+
 app.get('/records', (req, res) => {
   res.send(store.getRecords());
 });
-
 
 app.listen(PORT, () => {
   console.log('open music archive server started at http://localhost:' + PORT);
